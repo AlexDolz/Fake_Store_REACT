3f2aba03f6c8e2842fa192c58fe7ee841ca6926b
AlexDolz <aleksejdolznikov3@gmail.com>
2023-04-13 15:38:54 +0200

    hashrouter

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import HomePage from './components/Pages/HomePage/HomePage';
 import ProductsPage from './components/Pages/ProductsPage/ProductsPage';
 import ProductsInfoPage from './components/Pages/ProductsInfoPage/ProductsInfoPage';
@@ -12,15 +12,15 @@ import NotFoundPage from './components/Pages/NotFoundPage/NotFoundPage';
 const App = () => {
   return (
     <div>
-      <Router basename='/'>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Header />
-        <Routes>
-          <Route path='/Fake_Store_REACT' element={<HomePage />} />
-          <Route path='/products' element={<ProductsPage />} />
-          <Route path='/products/:id' element={<ProductsInfoPage />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
-      </Router>
+        <Switch>
+          <Route exact path='/' component={<HomePage />} />
+          <Route path='/products' component={<ProductsPage />} />
+          <Route path='/products/:id' component={<ProductsInfoPage />} />
+          <Route path='*' component={<NotFoundPage />} />
+        </Switch>
+      </HashRouter>
 
       <Footer />
     </div>
